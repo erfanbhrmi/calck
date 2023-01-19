9ec76b884c8f4e616a82d40edd634472f9bf29a8
erfan bahrami <erfanbahrami848484@gmail.com>
2023-01-19 15:04:11 +0330

    add project

---
+++ calculator.js
@@ -0,0 +1,63 @@
+function Dark() {
+    var element = document.body;
+    element.classList.toggle("Dark");
+    
+}
+
+function calck () {
+    var a = Number (document.getElementById("one").value);
+    var b = Number (document.getElementById("two").value);
+    document.getElementById("result").value = a+b;
+}
+
+function one() {
+    document.getElementById('one').style.backgroundColor='gray';
+} 
+function two() {
+    document.getElementById('two').style.backgroundColor='gray';
+} 
+function tree() {
+    document.getElementById('tree').style.backgroundColor='gray';
+} 
+function four() {
+    document.getElementById('four').style.backgroundColor='gray';
+} 
+function result() {
+    document.getElementById('result').style.backgroundColor='gray';
+} 
+function five() {
+    document.getElementById('five').style.backgroundColor='gray';
+} 
+function six() {
+    document.getElementById('six').style.backgroundColor='gray';
+} 
+function seven() {
+    document.getElementById('seven').style.backgroundColor='gray';
+} 
+function eight() {
+    document.getElementById('eight').style.backgroundColor='gray';
+} 
+function nine() {
+    document.getElementById('nine').style.backgroundColor='gray';
+} 
+function ten() {
+    document.getElementById('ten').style.backgroundColor='gray';
+} 
+function el() {
+    document.getElementById('el').style.backgroundColor='gray';
+} 
+function tw() {
+    document.getElementById('tw').style.backgroundColor='gray';
+} 
+function th() {
+    document.getElementById('th').style.backgroundColor='gray';
+} 
+function fo() {
+    document.getElementById('fo').style.backgroundColor='gray';
+} 
+function fi() {
+    document.getElementById('fi').style.backgroundColor='gray';
+} 
+function si() {
+    document.getElementById('si').style.backgroundColor='gray';
+} 
