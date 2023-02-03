0aa1d9eca181d352e40ae096c8c077a160a93a8a
erfan bahrami <erfanbahrami848484@gmail.com>
2023-02-03 15:15:20 +0330

    add project of clack

--- calculator.js
+++ calculator.js
@@ -1,63 +1,53 @@
 function Dark() {
     var element = document.body;
     element.classList.toggle("Dark");
-    
 }
 
-function calck () {
-    var a = Number (document.getElementById("one").value);
-    var b = Number (document.getElementById("two").value);
-    document.getElementById("result").value = a+b;
+function number1  () {
+var txt = document.getElementById('txtResult');
+ txt.value += '1';
 }
 
-function one() {
-    document.getElementById('one').style.backgroundColor='gray';
-} 
-function two() {
-    document.getElementById('two').style.backgroundColor='gray';
-} 
-function tree() {
-    document.getElementById('tree').style.backgroundColor='gray';
-} 
-function four() {
-    document.getElementById('four').style.backgroundColor='gray';
-} 
-function result() {
-    document.getElementById('result').style.backgroundColor='gray';
-} 
-function five() {
-    document.getElementById('five').style.backgroundColor='gray';
-} 
-function six() {
-    document.getElementById('six').style.backgroundColor='gray';
-} 
-function seven() {
-    document.getElementById('seven').style.backgroundColor='gray';
-} 
-function eight() {
-    document.getElementById('eight').style.backgroundColor='gray';
-} 
-function nine() {
-    document.getElementById('nine').style.backgroundColor='gray';
-} 
-function ten() {
-    document.getElementById('ten').style.backgroundColor='gray';
-} 
-function el() {
-    document.getElementById('el').style.backgroundColor='gray';
-} 
-function tw() {
-    document.getElementById('tw').style.backgroundColor='gray';
-} 
-function th() {
-    document.getElementById('th').style.backgroundColor='gray';
-} 
-function fo() {
-    document.getElementById('fo').style.backgroundColor='gray';
-} 
-function fi() {
-    document.getElementById('fi').style.backgroundColor='gray';
-} 
-function si() {
-    document.getElementById('si').style.backgroundColor='gray';
-} 
+var op;
+var number1;
+var number2;
+
+function Dark() {
+    var element = document.body;
+    element.classList.toggle("Dark");
+}
+
+function btnnumber  (e) {
+var txt = document.getElementById('txtResult');
+ txt.value += e.innerText;
+}
+
+
+function operation (e) {
+    op = e.innerText;
+    var txt = document.getElementById('txtResult');
+    number1 = parseInt(txt.value);
+    txt.value = '';
+}
+                                                               
+
+function calcy () {
+    var txt = document.getElementById('txtResult');
+    var result; 
+    number2 = parseInt(txt.value);
+
+
+    switch(op) {
+       case '+':  result = number1 + number2; break;
+       case '-':  result = number1 - number2; break;
+       case '/':  result = number1 / number2; break;
+       case '*':  result = number1 * number2; break;
+
+       default :  result='error';
+    }
+
+    txt.value = result;
+}
+
+
+document.getElementById('C') = '';
